3a3b2858d96e2a84fa831124bb22b5e126a0370f
Philip Kooistra <pkooistra@mpr.org>
2020-10-12 14:40:29 -0500

    Working ApmGallery. #76

--- src/nodes/ApmGallery.tsx
+++ src/nodes/ApmGallery.tsx
@@ -1,15 +1,32 @@
 import React, { FunctionComponent } from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { SliderBox } from 'react-native-image-slider-box';
+import { ApmComponentProps } from '../types';
+import { ImageAttrs } from '../types/prosemirror';
 
-const ApmGallery: FunctionComponent = () => {
-  return <Text style={styles.todo}>ApmGallery</Text>;
+const ApmGallery: FunctionComponent<ApmComponentProps> = (props) => {
+  const embeddedImages = props.nodeData.content.map((image) => {
+    const imageAttributes = image.attrs as ImageAttrs;
+    return props.embedded?.images?.find(
+      (embeddedImage) =>
+        embeddedImage.id && embeddedImage.id === imageAttributes.id
+    );
+  });
+
+  const images = embeddedImages.map(
+    (image) => image.preferred_aspect_ratio.instances[0].url
+  );
+
+  return (
+    <View style={styles.container}>
+      <SliderBox images={images} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-  todo: {
-    fontSize: 18,
-    fontStyle: 'italic',
-    color: 'crimson'
+  container: {
+    flex: 1
   }
 });
 
